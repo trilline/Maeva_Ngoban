3b20bffcfc83aaba3727d250cc930f083c933b7c
trilline <maevangoban@gmail.com>
2024-04-18 14:03:11 +0200

    essai résolution du pb tailwind dans le pipe et rgb is missing argument grenn

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   # GET /tasks/1 or /tasks/1.json
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,11 @@ Rails.application.configure do
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Cette configuration spécifie le compresseur CSS à utiliser lors de la compilation des assets. 
+  #En définissant la valeur sur nil, vous désactivez essentiellement la compression CSS pendant la compilation des assets. 
+  #Par défaut, Rails utilise le compresseur :sass ou :scss, mais en définissant cette option sur nil, vous utilisez la configuration par défaut.
+  config.assets.css_compressor = nil
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
